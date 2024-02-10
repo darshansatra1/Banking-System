25929e578a51eb966b7f64e88b42175d12f4e5d8
Darshan Satra <darshansatra1@gmail.com>
2024-02-10 15:19:08 -0500

    Set up for users authentication

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,11 @@
+const User = require("../models/userModel");
+const asyncHandler = require("express-async-handler");
+
+
+const createUser = asyncHandler(async (req, res) => {
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+});
+
+module.exports = { createUser, loginUser };
---
+++ Backend/models/UserModel.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose');
+const bcrypt = require("bcrypt");
+var validator = require('validator');
+
+var userSchema = new mongoose.Schema({
+    user_name: {
+        type: String,
+        required: [true, "Please Type A User Name!"],
+        trim: true,
+        validate: {
+            validator: function (v) {
+                let regex = new RegExp(
+                    "^(?=[a-zA-Z0-9._ ]{10,35}$)(?!.*[_.]{2})[^_.].*[^_.]$"
+                    /*   no >>> _ or . at the beginning
+                    no >>>__ or _. or ._ or .. inside 
+                    no >>> _ or . at the end
+                    [a-zA-Z0-9._] >> allowed characters
+                    username is {10-35} characters long
+                    */
+                );
+                return regex.test(v);
+            },
+            message: "Please Enter A Valid User Name!",
+        },
+    },
+    email: {
+        type: String,
+        required: [true, "Please Type An Email!"],
+        unique: true,
+        trim: true,
+        lowercase: true, // Normalize email to lowercase for case-insensitive comparisons
+        validate: {
+            validator: validator.isEmail,
+            message: 'Invalid email format'
+        }
+    },
+    password: {
+        type: String,
+        required: [true, "Please Type A Password!"],
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ['customer', 'merchant'],
+        default: 'customer'
+    },
+    user_status: {
+        type: Number,
+        default: 0, //active , 1 >> unactive, 2 >>suspended
+    },
+    //TODO: Add account schema
+}, {
+    timestamps: true
+});
+
+userSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSaltSync(process.SALT_ROUNDS);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.isPasswordMatched = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
+//Export the model
+module.exports = mongoose.model('User', userSchema);
