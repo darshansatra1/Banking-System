08b4ff10f6a8e7f1315644f375be198efafc20e5
Vaibhavi Patel <vakpatel@iu.edu>
2024-02-12 22:38:56 -0500

    e-bank cloning changes

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,60 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { AdminLoginPage } from "./views/admin/AdminLoginPage";
+import { UserLoginPage } from "./views/user/UserLoginPage";
+import { RegisterPage } from "./views/guest/RegisterPage";
+import { Index } from "./components/home/Index";
+import NotFoundPage from "./views/NotFound";
+
+
+function App() {
+
+    //User And Admin Paths
+    const paths = [
+      "/profile",
+      "/profile/:id",
+      "/profile/:id/update",
+      "/notifications",
+      "/notifications/:id",
+      "/account-request",
+      "/account/in/:id",
+      "/account/out/:id",
+      "/account/withdraw/:id",
+      "/account/transfer/:id",
+      "/account/deposit/:id",
+      "/account/deposit-logs/:id",
+      "/account/withdraw-logs/:id",
+      "/contact",
+      "/admins/profile/:id",
+      "/admins/profile/:id/update",
+    ];
+    
+  return (
+    <Router>
+      {/* Guest Routes */}
+      {/* {!user && !admin && ( */}
+        <Routes>
+          <Route index element={<Index />} />
+          <Route exact path="/register" element={<RegisterPage />} />
+          <Route exact path="/admins/login" element={<AdminLoginPage />} />
+          <Route exact path="/login" element={<UserLoginPage />} />
+          {paths.map((stringPath) => (
+            <Route
+              key={"Home"}
+              exact
+              path={stringPath}
+              element={<Navigate to={"/"} />}
+            />
+          ))}
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      {/* // )} */}
+    </Router>
+  );
+}
+
+export default App;
