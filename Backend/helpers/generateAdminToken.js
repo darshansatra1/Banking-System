f2b32e641f078bd4b59317dd36f580e1533243cc
Darshan Satra <darshansatra1@gmail.com>
2024-02-26 15:04:42 -0500

    Add expiry to jwt token

--- Backend/helpers/generateAdminToken.js
+++ Backend/helpers/generateAdminToken.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const generateAdminToken = (id, email, role) => {
-    return jwt.sign({ id, email, role }, process.env.JWT_SECRET);
+    return jwt.sign({ id, email, role }, process.env.JWT_SECRET,{expiresIn: '1h'});
 };
 
 module.exports = {
