20f8faea9edca1dc1d8744290c1467a2d9e1c55d
Darshan Satra <darshansatra1@gmail.com>
2024-02-28 14:58:27 -0500

    Create deposit, transfer and withdraw model

---
+++ Backend/models/TransferModel.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const transferSchema = new mongoose.Schema(
+    {
+        fromCustomer:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Customer',
+        },
+        toCustomer:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Customer',
+        },
+        fromMerchant:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Merchant',
+        },
+        toMerchant:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Merchant',
+        },
+        status:{
+            type: String,
+            enum:{
+                values:["waiting","accept","decline"],
+            }
+        },
+        balance_transferred: {
+            type: Number,
+            require: [true, "Please Provide Transferred Balance!"],
+            min: [1, "You Can Not Transfer Balance Less Than 1$"],
+        },
+    },
+    {
+        timestamps: true,
+        collection: "Transfers",
+    }
+);
+
+// Define Admin Model
+const Transfer = mongoose.model("Transfer", transferSchema);
+
+module.exports = Transfer;
