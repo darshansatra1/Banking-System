4460037f8159a2c01763e2b759f27a806532d08d
Darshan Satra <darshansatra1@gmail.com>
2024-02-29 17:58:36 -0500

    Add userlistview component to homeview.jsx

--- Frontend/src/components/shared/admin/UserListView.jsx
+++ Frontend/src/components/shared/admin/UserListView.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
-const UserListView = ({ user_name, email, balance, account_id, onClick }) => {
+const UserListView = ({ user_name, email, balance, account_id }) => {
+    const onClick = async()=>{
+
+    }
   return (
     <div className="user-card" onClick={onClick}>
       <h2>{user_name}</h2>
