bdd387b2c65f0aee4f8eabcb3f3748ee00f3b863
Sharma, Krishna Lalprasad <sharmakrish1130@gmail.com>
2024-04-11 17:22:30 -0400

    crypto

--- sbs-frontend/src/hooks/useLocalStorage.js
+++ sbs-frontend/src/hooks/useLocalStorage.js
@@ -1,26 +1,33 @@
+// useLocalStorage.js
 import { useState } from "react";
+import CryptoJS from "crypto-js";
 
-export const useLocalStorage = (keyName, defaultValue) => {
+export const useLocalStorage = (keyName, defaultValue, encryptionKey) => {
     const [storedValue, setStoredValue] = useState(() => {
         try {
             const value = window.localStorage.getItem(keyName);
             if (value) {
-                return JSON.parse(value);
-            } else {
-                window.localStorage.setItem(keyName, JSON.stringify(defaultValue));
-                return defaultValue;
+                // Decrypt the stored value
+                const decryptedValue = CryptoJS.AES.decrypt(value, encryptionKey).toString(CryptoJS.enc.Utf8);
+                return JSON.parse(decryptedValue);
             }
+            return {}
         } catch (err) {
+            console.error("Error handling localStorage:", err);
             return defaultValue;
         }
     });
+
     const setValue = (newValue) => {
         try {
-            window.localStorage.setItem(keyName, JSON.stringify(newValue));
+            // Encrypt and store the new value
+            const encryptedValue = CryptoJS.AES.encrypt(JSON.stringify(newValue), encryptionKey).toString();
+            window.localStorage.setItem(keyName, encryptedValue);
         } catch (err) {
-            console.log(err);
+            console.error("Error setting localStorage value:", err);
         }
         setStoredValue(newValue);
     };
+
     return [storedValue, setValue];
 };
--- sbs-frontend/src/pages/User/Create Bill/BillHistory.jsx
+++ sbs-frontend/src/pages/User/Create Bill/BillHistory.jsx
@@ -22,7 +22,16 @@ const BillHistory = () => {
                         Authorization: `Bearer ${user.token}`,
                     },
                 });
-                setBillHistory(response.data);
+
+
+
+
+
+
+
+
+
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++                setBillHistory(response.data);
                 setLoading(false);
             } catch (error) {
                 console.error('Error fetching bill history:', error);
