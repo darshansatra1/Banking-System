b033c854361660a16859fd4f4c16d3398d2dd2a6
Vaibhavi Patel <vakpatel@iu.edu>
2024-04-11 16:43:01 -0400

    admin profile page

--- sbs-frontend/src/pages/Admin/Profile/AdminProfilePage.jsx
+++ sbs-frontend/src/pages/Admin/Profile/AdminProfilePage.jsx
@@ -33,7 +33,42 @@ export const AdminProfilePage = () => {
         fetchUserData();
     }, []);
 
+
+    const extractDate = (datetimeString) => {
+      if (!datetimeString) return null; // handle null or undefined datetimeString
+      
+      const date = new Date(datetimeString);
+      
+      if (isNaN(date.getTime())) {
+          console.error("Invalid date string:", datetimeString);
+          return null;
+      }
+      
+      // Get the date part in yyyy-mm-dd format
+      return date.toISOString().split('T')[0];
+  };
+
+    const formattedDate = userData ? extractDate(userData.dob) : null;
+
     return (
-        <div>AdminProfilePage</div>
-    )
+      <div className="container mx-auto py-8">
+          <div className="max-w block p-6 rounded shadow-lg shadow-black/20 bg-gray-800 mx-auto">
+              <div className="p-6">
+                  <h3 className="flex justify-center items-center text-2xl text-blue-600 font-bold text-center p-2 my-4 rounded shadow bg-blue-200 border-x-4 select-none">
+                      <span>User Profile</span>
+                  </h3>
+                  {userData && (
+                  <div>
+                      <p className='text-slate-100'><strong>User Name:</strong> {userData.user_name}</p>
+                      <p className='text-slate-100'><strong>Email:</strong> {userData.email}</p>
+                      {formattedDate && <p className='text-slate-100'><strong>Date of Birth:</strong>{formattedDate}</p>}
+                      <p className='text-slate-100'><strong>Mobile Number:</strong> {userData.phone_number}</p>
+                      <p className='text-slate-100'><strong>Address:</strong> {userData.address}</p>
+                  </div>
+                  )
+                }
+              </div>
+          </div>
+      </div>
+  );
 }
